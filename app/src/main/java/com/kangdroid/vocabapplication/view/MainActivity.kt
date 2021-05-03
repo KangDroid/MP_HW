181a1ab590f8a5a5bdb5c74b72d6372eab7b4b37
KangDroid <hyunwoo286@gmail.com>
2021-05-03 22:47:31 +0900

    [MainActivity] Implement LoginFragment

--- app/src/main/java/com/kangdroid/vocabapplication/view/MainActivity.kt
+++ app/src/main/java/com/kangdroid/vocabapplication/view/MainActivity.kt
@@ -2,8 +2,11 @@ package com.kangdroid.vocabapplication.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.kangdroid.vocabapplication.R
 import com.kangdroid.vocabapplication.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 // The View
 @AndroidEntryPoint
@@ -14,8 +17,21 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    // Login Fragment
+    @Inject
+    lateinit var loginFragment: LoginFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+        commitFragment(loginFragment, false)
+    }
+
+    private fun commitFragment(targetFragment: Fragment, replace: Boolean) {
+        supportFragmentManager.beginTransaction().apply {
+            if (replace) addToBackStack(null)
+            replace(R.id.loginOrJoin, targetFragment)
+            commit()
+        }
     }
 }

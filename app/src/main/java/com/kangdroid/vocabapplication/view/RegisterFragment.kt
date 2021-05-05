3ddc3eeb9e6c418af43786a853a54050e1728153
KangDroid <hyunwoo286@gmail.com>
2021-05-05 17:07:48 +0900

    [RegisterFragment] Hook up register function to Fragment

--- app/src/main/java/com/kangdroid/vocabapplication/view/RegisterFragment.kt
+++ app/src/main/java/com/kangdroid/vocabapplication/view/RegisterFragment.kt
@@ -7,8 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.kangdroid.vocabapplication.R
 import com.kangdroid.vocabapplication.databinding.FragmentRegisterBinding
+import com.kangdroid.vocabapplication.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -17,6 +19,14 @@ class RegisterFragment @Inject constructor(): Fragment() {
     private val passwordRegex: Regex = """^(?=.*[@#$%!\-_?&])(?=\S+$).*""".toRegex()
     private var _registerFragmentBinding: FragmentRegisterBinding? = null
     private val fragmentRegisterBinding: FragmentRegisterBinding get() =_registerFragmentBinding!!
+
+    // Are we okay to proceed?
+    private var readyUserName: Boolean = false
+    private var readyUserPassword: Boolean = false
+    private var readyUserPasswordValidation: Boolean = false
+
+    // Login View Model
+    private val loginViewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,12 +42,27 @@ class RegisterFragment @Inject constructor(): Fragment() {
 
         // Init Text Layout
         initTextLayout()
+
+        // Init Register Button
+        initRegisterButton()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         // Make sure de-reference value since Memory leak might occurs.
         _registerFragmentBinding = null
+    }
+
+    private fun initRegisterButton() {
+        fragmentRegisterBinding.registerButton.setOnClickListener {
+            val isOkToGo: Boolean = readyUserName || readyUserPassword || readyUserPasswordValidation
+            if (isOkToGo) {
+                val userName: String = fragmentRegisterBinding.registerInputName.editText?.text.toString()
+                val userPassword: String = fragmentRegisterBinding.registerPasswordLayout.editText?.text.toString()
+                Log.d(this::class.java.simpleName, "UserName: $userName")
+                loginViewModel.registerUser(userName, userPassword)
+            }
+        }
     }
 
     private fun initTextLayout() {
@@ -66,14 +91,26 @@ class RegisterFragment @Inject constructor(): Fragment() {
     private fun checkUserName(userName: String) {
         if (userName.isEmpty()) {
             fragmentRegisterBinding.registerInputName.error = getString(R.string.required_field)
+            readyUserName = false
+        } else {
+            readyUserName = true
         }
     }
 
     private fun checkPassword(userPassword: String) {
         when {
-            (userPassword.length < 8) -> fragmentRegisterBinding.registerPasswordLayout.error = getString(R.string.password_length_short)
-            !passwordRegex.matches(userPassword) -> fragmentRegisterBinding.registerPasswordLayout.error = getString(R.string.password_no_special)
-            else -> fragmentRegisterBinding.registerPasswordLayout.error = null
+            (userPassword.length < 8) -> {
+                fragmentRegisterBinding.registerPasswordLayout.error = getString(R.string.password_length_short)
+                readyUserPassword = false
+            }
+            !passwordRegex.matches(userPassword) -> {
+                fragmentRegisterBinding.registerPasswordLayout.error = getString(R.string.password_no_special)
+                readyUserPassword = false
+            }
+            else -> {
+                fragmentRegisterBinding.registerPasswordLayout.error = null
+                readyUserPassword = true
+            }
         }
     }
 
@@ -84,8 +121,12 @@ class RegisterFragment @Inject constructor(): Fragment() {
         when {
             inputText != targetText -> {
                 fragmentRegisterBinding.registerPasswordCheck.error = getString(R.string.password_does_not_match)
+                readyUserPasswordValidation = false
             }
-            else -> fragmentRegisterBinding.registerPasswordCheck.error = null
+            else -> {
+                fragmentRegisterBinding.registerPasswordCheck.error = null
+                readyUserPasswordValidation = true
+            }
         }
     }
 }

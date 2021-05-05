a1aafbe22a35b857ee83045d2575b31aea836b4d
KangDroid <hyunwoo286@gmail.com>
2021-05-05 21:29:25 +0900

    [LoginFragment] Adapt Viewbinding on LoginFragment

--- app/src/main/java/com/kangdroid/vocabapplication/view/LoginFragment.kt
+++ app/src/main/java/com/kangdroid/vocabapplication/view/LoginFragment.kt
@@ -6,16 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.kangdroid.vocabapplication.R
+import com.kangdroid.vocabapplication.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class LoginFragment @Inject constructor(): Fragment() {
+    // View Binding
+    private var _fragmentLoginBinding: FragmentLoginBinding? = null
+    private val fragmentLoginBinding: FragmentLoginBinding get() = _fragmentLoginBinding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _fragmentLoginBinding = FragmentLoginBinding.inflate(inflater, container, false)
+        return fragmentLoginBinding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        // Make sure de-reference value since Memory leak might occurs.
+        _fragmentLoginBinding = null
     }
 }

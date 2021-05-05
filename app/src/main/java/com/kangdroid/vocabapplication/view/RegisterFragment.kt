6923345a8adbf8d67d5abf33c074822d840315ed
KangDroid <hyunwoo286@gmail.com>
2021-05-05 14:12:50 +0900

    [RegisterFragment] Adapt ViewBinding on RegisterFragment

--- app/src/main/java/com/kangdroid/vocabapplication/view/RegisterFragment.kt
+++ app/src/main/java/com/kangdroid/vocabapplication/view/RegisterFragment.kt
@@ -6,16 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.kangdroid.vocabapplication.R
+import com.kangdroid.vocabapplication.databinding.FragmentRegisterBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class RegisterFragment @Inject constructor(): Fragment() {
+    private var _registerFragmentBinding: FragmentRegisterBinding? = null
+    private val fragmentRegisterBinding: FragmentRegisterBinding get() =_registerFragmentBinding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        _registerFragmentBinding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return fragmentRegisterBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Make sure de-reference value since Memory leak might occurs.
+        _registerFragmentBinding = null
     }
 }

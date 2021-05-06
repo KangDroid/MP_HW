fca412ead1c7cd99d28d80a7eb69556740c5bbde
KangDroid <hyunwoo286@gmail.com>
2021-05-06 21:01:13 +0900

    [UserDetails] Prepare for party started :)

---
+++ app/src/main/java/com/kangdroid/vocabapplication/data/entity/userdetails/UserDetails.kt
@@ -0,0 +1,7 @@
+package com.kangdroid.vocabapplication.data.entity.userdetails
+
+import com.kangdroid.vocabapplication.data.entity.word.WordCategory
+
+class UserDetails {
+    var weakCategory: MutableList<WordCategory> = mutableListOf()
+}
